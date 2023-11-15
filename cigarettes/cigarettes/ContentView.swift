5bfde18268e9a5777208a26fbc2eab62b145eff7
Gianmarco Cremisi <marcomega0064@gmail.com>
2023-11-15 14:53:02 +0100

    first commit

---
+++ cigarettes/cigarettes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cigarettes
+//
+//  Created by Gianmarco Cremisi on 15/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cigarettes/cigarettes/cigarettesApp.swift
@@ -0,0 +1,17 @@
+//
+//  cigarettesApp.swift
+//  cigarettes
+//
+//  Created by Gianmarco Cremisi on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct cigarettesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
